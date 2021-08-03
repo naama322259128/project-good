bb41fcc12d58dbafbfcbff767b6b585ea1e7e9cb
Michalkatan18 <michalkatan18@gmail.com>
2021-08-03 17:10:22 +0300

    3.8 17:10 productsList

--- project/client/src/store/reducers/existingAuctionState.js
+++ project/client/src/store/reducers/existingAuctionState.js
@@ -2,9 +2,10 @@ import * as actionTypes from '../actionTypes';
 const initialState = {
 
     productsList: [
-        { code: "111", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 },
-        { code: "111", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 },
-        { code: "111", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 }
+        { cnt:1,   item:    { code: "111", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 }},
+        { cnt:4,   item:    { code: "222", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 }},
+        { cnt:18,   item:    { code: "333", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 }},
+        { cnt:3,   item:    { code: "444", name:"aaa", image: "465", description: "osjqw jrngu3i", price: 15 }}
     ],//רשימת מוצרים של מכירה זו
 
 }
