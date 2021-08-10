b931e22dd6ab776c63f63201898dfce6ab0f9a22
Michalkatan18 <michalkatan18@gmail.com>
2021-08-10 17:18:33 +0300

    images 17:18

--- project/client/src/store/reducers/mainState.js
+++ project/client/src/store/reducers/mainState.js
@@ -1,11 +1,17 @@
 import * as actionTypes from '../actionTypes';
+import p1 from '../../img/pic1.png'
+import p2 from '../../img/pic2.jpg'
+import p3 from '../../img/pic3.png'
+import p4 from '../../img/pic4.jpg'
+
+
 const initialState = {
     //רשימת המכירות באתר
     auctionsList: [
 //כשנתחבר לסרבר לא נצטרך את מערך המוצרים כאן, כי נשלוף לפי הקוד רק למכירה שהלקוח בוחר
 
         {
-            code: "11", name: "Ezer Mitzion",
+            code: "11", name: "Ezer Mitzion",image:p1,
             //auctionManager: "stam code menahel",
             //lotteriesDate: "18/01/2022",   //תאריך ביצוע ההגרלות
             //registrationEndDate:"17/01/2022",
@@ -21,7 +27,7 @@ const initialState = {
 
 
         {
-            code: "22", name: "Seminar Elkayam",
+            code: "22", name: "Seminar Elkayam",image:p2,
             productList: [
                 { code: "1", name: "chair", image: "465", description: "vary good chair", price: 15 },
                 { code: "2", name: "table", image: "465", description: "table from IKEA", price: 15 },
@@ -31,7 +37,7 @@ const initialState = {
 
 
         {
-            code: "33", name: "Ihud Hatzala",
+            code: "33", name: "Ihud Hatzala",image:p3,
             productList: [
                 { code: "11155", name: "CHEMISE 1000$", image: "465", description: "nice store", price: 14 },
                 { code: "22255", name: "aaa", image: "465", description: "osjqw jrngu3i", price: 43 },
@@ -41,7 +47,7 @@ const initialState = {
 
 
         {
-            code: "44", name: "Od Yosef Hay",
+            code: "44", name: "Od Yosef Hay",image:p4,
             productList: [
                 { code: "11187", name: "apple", image: "465", description: "Od Yosef Hay osjqw jrngu3i", price: 10 },
                 { code: "22287", name: "mango", image: "465", description: "Od Yosef Hay osjqw jrngu3i", price: 20 },
