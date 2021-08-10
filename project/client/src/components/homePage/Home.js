9c528916250cf4dcc4d8df7c6951acb0fa7cf65f
Michalkatan18 <michalkatan18@gmail.com>
2021-08-10 15:52:12 +0300

    window.scrollTo(0, 1000)

--- project/client/src/components/homePage/Home.js
+++ project/client/src/components/homePage/Home.js
@@ -16,7 +16,7 @@ const Home = (props) => {
     {/* log-out להוסיף כפתור התנתקות */}
     <header>
       <Link to={"/home"}>  <div className="logo" ></div></Link>
-      <Button type="button" className="btnLogin btn" onClick={() => {  window.scrollTo(500, 0); props.setLogin(true); }}>Login</Button>
+      <Button type="button" className="btnLogin btn" onClick={() => {  window.scrollTo(0, 1000); props.setLogin(true); }}>Login</Button>
       <p>Back to<br />School</p>
       <Button type="button" className="btnMoreInfo btn">More Info</Button>
       <Link to={"/newAuction"}><Button type="button" className="btn">NEW AUCTION</Button></Link>
