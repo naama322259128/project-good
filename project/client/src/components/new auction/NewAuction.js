86916fa50e5ea338db6a7c7ff0403f52b19121ab
Michalkatan18 <michalkatan18@gmail.com>
2021-08-11 20:46:43 +0300

    20:46

--- project/client/src/components/new auction/NewAuction.js
+++ project/client/src/components/new auction/NewAuction.js
@@ -8,42 +8,36 @@ import { BrowserRouter as Router, Link, useRouteMatch, Route, Switch } from 'rea
 import FinalStep from './FinalStep';
 import './NewAuction.scss';
 const NewAuction = (props) => {
-    return (<>
-       <header className="new_auction_header">
+    return (<div id="new_auction">
+
+        <header className="new_auction_header">
             {/* log-out להוסיף כפתור התנתקות */}
-
-
-
             <Link to={"/home"}>  <div className="logo" ></div></Link>
         </header>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <h1>New Auction</h1>
-        {/* ארבעת המלבנים */}
-        <Router>
-            <Switch>
-                <Route path="/newAuction/2"><UploadingProducts /></Route>
-                <Route path="/newAuction/3"><OrganizationInformation /></Route>
-                <Route path="/newAuction/4"><AuctionInformation /></Route>
-                <Route path="/newAuction/"><AuctionPricing /></Route>
-            </Switch>
-        </Router>
 
-        {props.isOpen ? <FinalStep /> : null}        {/* אישור סופי */}
+        <section id="new_auction_section">
+            <h1>New Auction</h1>
+            {/* ארבעת המלבנים */}
+            <Router>
+                <Switch>
+                    <Route path="/newAuction/2"><UploadingProducts /></Route>
+                    <Route path="/newAuction/3"><OrganizationInformation /></Route>
+                    <Route path="/newAuction/4"><AuctionInformation /></Route>
+                    <Route path="/newAuction/"><AuctionPricing /></Route>
+                </Switch>
+            </Router>
+            {props.isOpen ? <FinalStep /> : null}        {/* אישור סופי */}
+        </section>
+
         <footer className="new_auction_footer"></footer>
 
-    </>
+    </div>
     )
 }
 const mapStateToProps = (state) => {
     return {
         isOpen: state.auction.finalStepModalIsOpen,
-        stage:state.auction.stage//איזה שלב אנחנו תבהליך
+        stage: state.auction.stage//איזה שלב אנחנו תבהליך
     };
 }
 export default connect(mapStateToProps, {})(NewAuction);
