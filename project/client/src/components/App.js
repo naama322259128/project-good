55dd50dac3ee6c4031bc63a17ff14585e4e628c5
naama322259128 <82829409+naama322259128@users.noreply.github.com>
2021-08-11 17:21:34 +0300

    17:21

--- project/client/src/components/App.js
+++ project/client/src/components/App.js
@@ -4,20 +4,19 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Auction from '../components/auction/Auction';
 import NewAuction from '../components/new auction/NewAuction';
 import About from './main/About'
-import Profile from'./login/profile/Profile';
-function App() {    // <Router>
-    //   <div>
-    //     <Switch>
-    //       <Route path={`/auction`}><Auction /></Route>
-    //       <Route path={`/home`}><Home /></Route>
-    //       <Route path={`/newAuction`}><NewAuction/></Route>        
-    //       <Route path={`/about`}><About/></Route>        
-    //       <Route exact path={`/`}><Home /></Route>
-    //     </Switch>
-    //   </div>
-    // </Router>
+function App() {
   return (
-<Profile/>
+    <Router>
+      <div>
+        <Switch>
+          <Route path={`/auction`}><Auction /></Route>
+          <Route path={`/home`}><Home /></Route>
+          <Route path={`/newAuction`}><NewAuction/></Route>        
+          <Route path={`/about`}><About/></Route>        
+          <Route exact path={`/`}><Home /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- project/client/src/components/homePage/Home.js
+++ project/client/src/components/homePage/Home.js
@@ -9,15 +9,67 @@ import CurrentAuction from '../auction/CurrentAuction';
 import Cart from '../auction/Cart';
 import Button from '@material-ui/core/Button';
 import HomeFooter from './HomeFooter';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+const options = [
+  'Your profile',
+  'Sign out'
+];
+const ITEM_HEIGHT = 48;
 
 const Home = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (<>
 
-    {/* log-out להוסיף כפתור התנתקות */}
+
     <header className="home_header">
 
       <Link to={"/home"}>  <div className="logo" ></div></Link>
+      {
+        props.currentUser?     
+    <div>
+      <AccountCircleIcon
+        aria-label="more"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </AccountCircleIcon>
+     
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>:null}
    {props.currentUser?null: <Button type="button" className="btnLogin btn" onClick={() => { window.scrollTo(0, 1000); props.setLogin(true); }}>Login</Button>}  
       <div id="home_text">
         <h3>Build your </h3>
