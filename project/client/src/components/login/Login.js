cd62c5217faa3e9f8e8fd0a9747a9cc7ee759c47
naama322259128 <82829409+naama322259128@users.noreply.github.com>
2021-08-15 00:22:30 +0300

    00:22

--- project/client/src/components/login/Login.js
+++ project/client/src/components/login/Login.js
@@ -1,3 +1,7 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import React, { useState } from 'react';
 import './Login.scss';
 import SignUp from './SignUp'
@@ -5,7 +9,20 @@ import SignIn from './SignIn'
 import { setLogin } from '../../store/actions/home'
 import { connect } from "react-redux";
 
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
 const Login = (props) => {
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const [show, setShow] = useState(<SignIn />);
 
@@ -22,8 +39,17 @@ const Login = (props) => {
     <center>
       <div id="myModal" className="modal_login glass_login" onClick={() => props.setLogin(false)} >
         <div className="modal-content_login" onClick={click}>
-          <div id="btn_signin" onClick={signInShow} >SIGN IN</div>
-          <div id="btn_signup" onClick={signUpShow}>SIGN UP</div>
+    <Tabs
+      value={value}
+      onChange={handleChange}
+      indicatorColor="primary"
+      textColor="primary"
+      centered
+    >
+      <Tab label="SIGN IN"  id="btn_signin" onClick={signInShow}/>
+      <Tab label="SIGN UP" id="btn_signup" onClick={signUpShow}/>
+
+    </Tabs>
           <div id="login_top" >
             {show}
           </div>
