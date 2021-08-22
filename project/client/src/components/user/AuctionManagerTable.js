184f078ba01e61f2a5a602ff17021ad3aec90485
naama322259128 <82829409+naama322259128@users.noreply.github.com>
2021-08-22 17:38:12 +0300

    17:38

--- project/client/src/components/user/AuctionManagerTable.js
+++ project/client/src/components/user/AuctionManagerTable.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment'
-<<<<<<< HEAD
-=======
 import Button from '@material-ui/core/Button';
 import './tables.scss'
->>>>>>> 18880aaa807e48c9c8b1b7c1f914d6db0060cef4
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
