dbf1b474eb141747133f69b2d684d9add251e45f
Michalkatan18 <michalkatan18@gmail.com>
2021-10-06 21:15:28 +0300

    21:15

--- project/Server/routes/order.js
+++ project/Server/routes/order.js
@@ -1,15 +1,15 @@
-const express=require("express");
-const route=express.Router();
-const orderController=require("../controllers/order");
+const express = require("express");
+const route = express.Router();
+const orderController = require("../controllers/order");
 
 
-route.get("/",orderController.getAll)
-route.get("/:id",orderController.getById)
-route.get("/orderList/:id",orderController.getOrderListByIdUser)
-route.get("/:user_id&:auction_id",orderController.getOrderByToUserCodeAndAuction)
-route.post("/",orderController.addOrder)
-route.delete("/:id",orderController.deleteOrder)
+route.get("/", orderController.getAll)
+route.get("/:id", orderController.getById)
+route.get("/orderList/:id", orderController.getOrderListByIdUser)
+route.get("/:user_id&:auction_id", orderController.getOrderByToUserCodeAndAuction)
+route.post("/", orderController.addOrder)
+route.delete("/:id", orderController.deleteOrder)
 
-route.get("/max")
+route.get("/soldMostTickets", orderController.getAuctionSoldMostTickets)
 
-module.exports=route;
+module.exports = route;
