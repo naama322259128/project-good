4cf6ffca8704ee6d73606994329eb094df02f932
Michalkatan18 <michalkatan18@gmail.com>
2021-10-26 18:08:12 +0300

    18:07

--- project/client/src/components/App.js
+++ project/client/src/components/App.js
@@ -21,21 +21,20 @@ function App() {
     <Router>
       <Switch>
         {/* <AuctionManagerTable/> */}
-        <StateCounter/>
+        {/* <StateCounter/> */}
         {/* <UserTable/> */}
         {/* <ContactForm/> */}
         {/* <NewAuction /> */}
         {/* <UpdateDetails/> */}
 
-        {/* <Route path={`/auction`} ><Auction /></Route>
+        <Route path={`/auction`} ><Auction /></Route>
         <Route path={`/home`}><Home /></Route>
         <Route path={`/new_auction`}><NewAuction /></Route>
         <Route path={`/continueNewAuction`}><ContinueNewAuction /></Route>
         <Route path={`/about`}><About /></Route>
         <Route path={`/your_profile`}><YourProfile /></Route>
         <Route path={`/update_your_details`}><UpdateDetails /></Route>
-        <Route path={`/`}><Home />    </Route> */}
-{/* 22 */}
+        <Route path={`/`}><Home />    </Route>
       </Switch>
     </Router>
 
