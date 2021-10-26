927eef3c9781088e778f7d66b5c94c26950766f0
Michalkatan18 <michalkatan18@gmail.com>
2021-10-26 11:08:54 +0300

    11:08

--- project/client/src/components/App.js
+++ project/client/src/components/App.js
@@ -26,10 +26,6 @@ function App() {
         {/* <ContactForm/> */}
         {/* <NewAuction /> */}
         {/* <UpdateDetails/> */}
-<<<<<<< HEAD
-=======
-{/* <ContactForm/> */}
->>>>>>> 58c819bfc053797af0066c7fa3d278ea6ddc79ce
 
         <Route path={`/auction`} ><Auction /></Route>
         <Route path={`/home`}><Home /></Route>
--- project/client/src/components/user/LoginGoogle.js
+++ project/client/src/components/user/LoginGoogle.js
@@ -6,30 +6,23 @@ import { userReducer as reducer, initialState as userState } from '../../store/r
 import * as actionTypes from '../../store/actionTypes';
 
 const LoginGoogle = () => {
-<<<<<<< HEAD
-=======
   const [state, dispatch, writeError] = useStorageReducer(
     localStorage,
     'user',
     reducer,
     userState
   );
->>>>>>> 58c819bfc053797af0066c7fa3d278ea6ddc79ce
 
   const responseGoogle = (response) => {
     console.log(response);
     const name = response.profileObj.name;
     const email = response.profileObj.email;
-<<<<<<< HEAD
-    loginGoogle(name, email);
-=======
     loginGoogle(name, email).then(succ=>{
       dispatch({
         type: actionTypes.SET_CURRENT_USER,
         payload: succ.data  
       })
     })
->>>>>>> 58c819bfc053797af0066c7fa3d278ea6ddc79ce
   }
 
   return (
